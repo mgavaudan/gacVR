023f37924d9b171926fd71239d15e9c9ea2e7b22
Matthieu Gavaudan <matthieugavaudan@Matthieus-MacBook-Air.local>
2015-05-30 16:32:16 -0400

    back to obj loader

--- website/js/main.js
+++ website/js/main.js
@@ -1,45 +1,180 @@
-window.onload=function(){
-  init();
-  animate();
-}
+var container, stats;
+var camera, scene, projector, raycaster, renderer;
+var vrEffect;
+var vrControls;
+var fullScreenButton = document.querySelector( '.button' );
+
+var mouse = new THREE.Vector2(), INTERSECTED;
+var radius = 100, theta = 0;
+
+init();
+animate();
 
 function init() {
 
-  container = document.getElementById( 'container' );
-  console.log(container)
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-  camera.position.z = 100;
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-  scene = new THREE.Scene();
+	var info = document.createElement( 'div' );
+	info.style.position = 'absolute';
+	info.style.top = '10px';
+	info.style.width = '100%';
+	info.style.textAlign = 'center';
+	info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> webgl - interactive cubes';
+	container.appendChild( info );
 
-  var ambient = new THREE.AmbientLight( 0x101030 );
-  scene.add( ambient );
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
 
-  var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-  directionalLight.position.set( 0, 0, 1 );
-  scene.add( directionalLight );
+	scene = new THREE.Scene();
 
-  var loader = new THREE.OBJLoader();
-  loader.load( 'model.obj', function ( object ) {
-    console.log(object);
-    scene.add( object );
-  } );
+	var light = new THREE.DirectionalLight( 0xffffff, 2 );
+	light.position.set( 1, 1, 1 ).normalize();
+	scene.add( light );
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  container.appendChild( renderer.domElement );
+	var light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( -1, -1, -1 ).normalize();
+	scene.add( light );
+
+
+	//***********************  Model ********************************//
+	//***************************************************************//
+
+	var geometry = new THREE.IcosahedronGeometry(1, 1, 1);
+
+	for ( var i = 0; i < 10; i ++ ) {
+
+		var obj = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+		obj.position.x = Math.random() * 800 - 400;
+		obj.position.y = Math.random() * 800 - 400;
+		obj.position.z = Math.random() * 800 - 400;
+
+		obj.rotation.x = Math.random() * 2 * Math.PI;
+		obj.rotation.y = Math.random() * 2 * Math.PI;
+		obj.rotation.z = Math.random() * 2 * Math.PI;
+
+		obj.scale.x = Math.random()*15 + 5;
+		obj.scale.y = Math.random()*15 + 5;
+		obj.scale.z = Math.random()*15 + 5;
+
+		scene.add( obj );
+
+	}
+
+	var loader = new THREE.ObjectLoader();
+		loader.load("/scene.json",function ( object ) {
+		scene.add( object );
+	});
+
+
+	//***************************************************************//
+	//***************************************************************//
+
+	projector = new THREE.Projector();
+	raycaster = new THREE.Raycaster();
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+
+	var fullScreenButton = document.querySelector( '.button' );
+	fullScreenButton.onclick = function() {
+		vrEffect.setFullScreen( true );
+	};
+
+	vrEffect = new THREE.VREffect(renderer, VREffectLoaded);
+	vrControls = new THREE.VRControls(camera);
+	function VREffectLoaded(error) {
+		if (error) {
+			fullScreenButton.innerHTML = error;
+			fullScreenButton.classList.add('error');
+		}
+	}
+
+	renderer.setClearColor( 0xf0f0f0 );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.sortObjects = false;
+	container.appendChild( renderer.domElement );
+
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	container.appendChild( stats.domElement );
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+	//
+
+	window.addEventListener( 'resize', onWindowResize, false );
 
 }
 
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	vrEffect.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	event.preventDefault();
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+//
+
 function animate() {
 
-  requestAnimationFrame( animate );
-  render();
+	requestAnimationFrame( animate );
+
+	render();
+	stats.update();
 
 }
 
 function render() {
 
-  renderer.render( scene, camera );
+	theta += 0.1;
+
+	camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
+	camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
+	camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
+	camera.lookAt( scene.position );
+
+	// find intersections
+
+	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+	projector.unprojectVector( vector, camera );
+
+	raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
+
+	var intersects = raycaster.intersectObjects( scene.children );
+
+	if ( intersects.length > 0 ) {
+
+		if ( INTERSECTED != intersects[ 0 ].object ) {
+
+			if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+			INTERSECTED = intersects[ 0 ].object;
+			INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+			INTERSECTED.material.emissive.setHex( 0xff0000 );
+
+		}
+
+	} else {
+
+		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+		INTERSECTED = null;
+
+	}
+
+	vrControls.update();
+	vrEffect.render( scene, camera );
 
 }
